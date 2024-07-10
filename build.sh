08ecdb694975dc4d5da31f645722d526982379bc
Rob Atkinson <ratkinson@ogc.org>
2024-07-10 10:59:37 +1000

    local build scripts

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 # Process building blocks
-docker run --pull=always --rm --workdir /workspace -v "$(pwd):/workspace" ghcr.io/opengeospatial/bblocks-postprocess  --clean true
+docker run --pull=always --rm --workdir /workspace -v "$(pwd):/workspace" ghcr.io/opengeospatial/bblocks-postprocess  --clean true --base-url http://localhost:9090/register/
